64aa04d702014f14bf8ddab6fc3aff156051a111
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-01-03 17:19:44 +0100

    added two options to make Web API find the newly created controller in the separate assembly

--- source/DotNetBay.SelfHost/Startup.cs
+++ source/DotNetBay.SelfHost/Startup.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Web.Http;
 using DotNetBay.Health.Owin;
+using DotNetBay.WebApi;
 using Microsoft.Owin;
 using Owin;
 
@@ -18,8 +18,27 @@ namespace DotNetBay.SelfHost
             var httpConfig = new HttpConfiguration();
 
             httpConfig.MapHttpAttributeRoutes();
+
+            // Option 1: Make sure the assembly is loaded in the current Context with the typeof()-"Hack"
+            var type = typeof(StatusController);
+
+            // Option 2: Custom Assemblies Resolver to load additional Assemblies when searching for Controllers
+            // httpConfig.Services.Replace(typeof(IAssembliesResolver), new MyNewAssembliesResolver());
+
             app.UseWebApi(httpConfig);
 
         }
+
+        //public class MyNewAssembliesResolver : DefaultAssembliesResolver
+        //{
+        //    public override ICollection<Assembly> GetAssemblies()
+        //    {
+        //        var baseAssemblies = base.GetAssemblies();
+        //        var controllersAssembly = Assembly.LoadFrom("DotNetBay.WebApi.dll");
+        //        baseAssemblies.Add(controllersAssembly);
+
+        //        return baseAssemblies;
+        //    }
+        //}
     }
 }
