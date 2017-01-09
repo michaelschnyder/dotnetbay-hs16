b4fa28ac986078764787ebdc6a5d001fa69f9f1d
Michael Schnyder <michael.schnyder@outlook.com>
2017-01-09 23:45:23 +0100

    added API to MVC WebApp

---
+++ source/DotNetBay.WebApp/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Http;
+
+using DotNetBay.WebApp;
+
+using Microsoft.Owin;
+
+using Owin;
+using DotNetBay.WebApi;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace DotNetBay.WebApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var type = typeof(StatusController);
+
+            // Configure Web API for self-host. 
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            app.UseWebApi(config);
+        }
+    }
+}
