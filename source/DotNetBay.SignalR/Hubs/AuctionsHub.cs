8cbbf8465198caa86522e656f3efdf8d44aa1ae1
Michael Schnyder <michael.schnyder@outlook.com>
2017-01-11 23:02:43 +0100

    added hub class as a wrapper to forward events to client

---
+++ source/DotNetBay.SignalR/Hubs/AuctionsHub.cs
@@ -0,0 +1,28 @@
+﻿using DotNetBay.Data.Entity;
+using Microsoft.AspNet.SignalR;
+
+namespace DotNetBay.SignalR.Hubs
+{
+    public class AuctionsHub : Hub
+    {
+        public static void NotifyNewAuction(Auction auction)
+        {
+            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.NewAuction(auction.Id, auction);
+        }
+
+        public static void NotifyBidAccepted(Auction auction, Bid bid)
+        {
+            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.BidAccepted(auction.Id, bid.Id, auction, bid);
+        }
+
+        public static void NotifyAuctionStarted(Auction auction)
+        {
+            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.AuctionStarted(auction.Id, auction);
+        }
+
+        public static void NotifyAuctionEnded(Auction auction)
+        {
+            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.AuctionStarted(auction.Id, auction);
+        }
+    }
+}
