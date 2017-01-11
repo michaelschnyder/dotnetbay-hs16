16b6d97fc348a0c33c69e87e37fad4e65c2b0bf7
Michael Schnyder <michael.schnyder@outlook.com>
2017-01-11 23:52:38 +0100

    completed signalR

--- source/DotNetBay.SignalR/Hubs/AuctionsHub.cs
+++ source/DotNetBay.SignalR/Hubs/AuctionsHub.cs
@@ -1,4 +1,5 @@
-﻿using DotNetBay.Data.Entity;
+﻿using System.Threading.Tasks;
+using DotNetBay.Data.Entity;
 using Microsoft.AspNet.SignalR;
 
 namespace DotNetBay.SignalR.Hubs
@@ -7,27 +8,33 @@ namespace DotNetBay.SignalR.Hubs
     {
         public static void NotifyNewAuction(Auction auction)
         {
-            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.NewAuction(auction.Id);
+            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.NewAuction(auction.Id, auction.Title, auction.StartPrice);
         }
 
         public static void NotifyNewBid(Auction auction, Bid bid)
         {
-            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.NewBid(auction.Id, bid.Id);
+            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.NewBid(auction.Id, auction.Title, bid.Amount, bid.Bidder.DisplayName);
         }
 
         public static void NotifyBidAccepted(Auction auction, Bid bid)
         {
-            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.BidAccepted(auction.Id, bid.Id);
+            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.BidAccepted(auction.Id, auction.Title, bid.Amount, bid.Bidder.DisplayName);
         }
 
         public static void NotifyAuctionStarted(Auction auction)
         {
-            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.AuctionStarted(auction.Id);
+            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.AuctionStarted(auction.Id, auction.Title);
         }
 
         public static void NotifyAuctionEnded(Auction auction)
         {
-            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.AuctionStarted(auction.Id);
+            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.AuctionStarted(auction.Id, auction.Title);
+        }
+
+        public override Task OnConnected()
+        {
+            this.Clients.Caller.AuctionStarted(42, "hahaqh");
+            return base.OnConnected();
         }
     }
 }
